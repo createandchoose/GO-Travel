9467c09482d2e53514c85aaa288b9ffeb15745d7
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2024-11-24 13:01:51 +0900

    asd

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "https://trips-travel-server.vercel.app/api";
-const BASE_URL = "http://localhost:3050/api";
+const BASE_URL = "http://176.57.208.64:3050";
 //
 export default BASE_URL;
