aefce0cd56f40a7e5d45bfbb4f5c5cc17f6e75df
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2024-11-24 13:04:40 +0900

    asd

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "https://trips-travel-server.vercel.app/api";
-const BASE_URL = "http://176.57.208.64:3050";
+const BASE_URL = "https://createandchoose-go-travel-8520.twc1.net/";
 //
 export default BASE_URL;
